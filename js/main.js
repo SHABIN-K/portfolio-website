8109b844d3e1b44f38653bcf85ccfcdc38f7c07c
Shabin-k <helloshabink@gmail.com>
2022-10-26 23:29:35 +0530

    main files

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+} 
+
+// nav hide 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+/*
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  btn.classList.add("clicked");
+  setTimeout(() => {
+    btn.classList.remove("clicked");
+  }, 4000);
+});
+
+*/
